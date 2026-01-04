4ccf22d822ece1fab222cc8f601a56d7489335fc
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2026-01-04 17:18:18 +0700

    add jwt key for env.example

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,8 +1,8 @@
 export default () => ({
   jwt: {
     secret: process.env.JWT_SECRET || 'your-secret-key',
-    expiresIn: process.env.JWT_EXPIRES_IN || '7d',
-    refreshSecret: process.env.JWT_REFRESH_SECRET,
-    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '30d',
+    expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN || '15m',
+    refreshSecret: process.env.JWT_REFRESH_SECRET || 'your-refresh-secret-key',
+    refreshExpiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRES_IN || '7d',
   },
 });
